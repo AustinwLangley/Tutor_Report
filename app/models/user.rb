f9650288a5cb5e9594f5685c13888602c3cd50ce
Austin Langley <austinwlangley@gmail.com>
2015-04-24 11:37:49 -0700

    change vaildation to only apply to when first saving a password and allow for saving changes made in the terminal

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   #This vaidates the name, email and password
   validates :name, presence: true #make sure name is present
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i } #make sure email is present, unique and in the correct format
-  validates :password, presence: true, confirmation: true, length: { in: 6..20} #makes sure password is present, confirmed and 6 to 20 characters   
+  validates :password, presence: true, confirmation: true, length: { in: 6..20}, on: :create #makes sure password is present, confirmed and 6 to 20 characters   
 
 
 	def password=(unencrypted_password)
