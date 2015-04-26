f8dcb04e328efffb155f0702480d66bdf1c87a36
Austin Langley <austinwlangley@gmail.com>
2015-04-26 15:02:46 -0700

    added the correct time to the created at section of the report

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
 
   #helper method current_user is defined here so that all controllers will have access to it.
   helper_method :current_user
-
+  #set time zone to Pacific time for the strftime method.
+  Time.zone = "Pacific Time (US & Canada)"
+	
 	private 
 	def current_user #helper method that helps with identifying whether a user is logged in.
 	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
